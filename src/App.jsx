cdef7a7e467688b7f5256f26cfe492ff561674a9
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-11-10 12:46:27 +0100

    milestone1: create article list

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
 function App() {
+
+  const articles = [
+    {
+      id: 1,
+      title: "Calcio"
+    },
+    {
+      id: 2,
+      title: "Cronaca nera"
+    },
+    {
+      id: 3,
+      title: "Politica"
+    },
+    {
+      id: 4,
+      title: "Tecnologia"
+    },
+    {
+      id: 5,
+      title: "Salute"
+    }
+  ]
+
   return (
     <>
-    
+      <div className="container">
+        <h1 className="text-center py-4">React Blog Form</h1>
+        <ul className="list-group">
+          {articles.map((article) => (
+            <li className="list-group-item">{article.title}</li>
+        ))}
+        </ul>
+      </div>
     </>
   )
 }
